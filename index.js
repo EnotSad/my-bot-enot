3c6427a2179e3f8c25c82d0746465ae35e5f6bf7
EnotSad <35348681+EnotSad@users.noreply.github.com>
2018-01-12 21:35:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const TelegramBot = require('node-telegram-bot-api')
+var TelegramBot = require('node-telegram-bot-api')
+    TOKEN = '507418737:AAHmDGQ5U_0oV-vnsre_Q84JjTSww8xyEwk';
 
 
+var bot = new TelegramBot (TOKEN, {polling: true});
 
-const TOKEN = '507418737:AAHmDGQ5U_0oV-vnsre_Q84JjTSww8xyEwk'
-
-
-const bot = new TelegramBot (TOKEN, {polling: true})
-
-bot.on ('message', msg => {
-    bot.sendMessage(msg.chat.id, 'Привет, как тебя зовут')
+bot.on ('message', function(msg) {
+    var id = msg.from.id;
+    var priem = msg.text
+    if priem in 'Привет':
+        bot.sendMessage(msg.chat.id, 'Привет, как тебя зовут')
 })
 
 
