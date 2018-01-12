0dd8b34566d9102dcf05495bcedfdf5a5103748c
EnotSad <35348681+EnotSad@users.noreply.github.com>
2018-01-12 20:22:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const TOKEN = '507418737:AAHmDGQ5U_0oV-vnsre_Q84JjTSww8xyEwk'
 const bot = new TelegramBot (TOKEN, {polling: true})
 
 bot.on ('message', msg => {
-    bot.sendMessage(msg.chat.id, 'Hello from HEROKU')
+    if message.text == "Привет":
+        bot.sendMessage(msg.chat.id, 'Привет, как тебя зовут')
 })
 
 
