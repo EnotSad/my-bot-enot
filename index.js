8fb3072f82bda68800ba1d5dc647e4d6a24b6df6
EnotSad <35348681+EnotSad@users.noreply.github.com>
2018-01-13 11:37:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,27 +9,28 @@ var bot = new TelegramBot (TOKEN, {polling: true});
 bot.onText (/Привет/, msg => {
     var id = msg.from.id;
     bot.sendMessage(msg.chat.id, 'Привет, как тебя зовут')
+})
 bot.onText (/БГУИР/, msg => {
     var id = msg.from.id;
-    bot.sendMessage(msg.chat.id, 'У БГУИРа одна потеха, сосать х.. у Политеха')
+    bot.sendMessage(msg.chat.id, 'У БГУИРа одна потеха, сосать х.. у Политеха')})
 bot.onText (/Политех/, msg => {
     var id = msg.from.id;
-    bot.sendMessage(msg.chat.id, 'Политех? Туда ещё поступают?')
+    bot.sendMessage(msg.chat.id, 'Политех? Туда ещё поступают?')})
 bot.onText (/Смол/, msg => {
     var id = msg.from.id;
-    bot.sendMessage(msg.chat.id, 'Он ведь не говорит р')
+    bot.sendMessage(msg.chat.id, 'Он ведь не говорит р')})
 bot.onText (/Юнкевич/, msg => {
     var id = msg.from.id;
-    bot.sendMessage(msg.chat.id, 'Недозамок')
+    bot.sendMessage(msg.chat.id, 'Недозамок')})
 bot.onText (/Какое сейчас время\?/, msg => {
     var id = msg.from.id;
-    bot.sendMessage(msg.chat.id, 'BQADAgADeAcAAlOx9wOjY2jpAAHq9DUC')
+    bot.sendMessage(msg.chat.id, 'BQADAgADeAcAAlOx9wOjY2jpAAHq9DUC')})
 bot.onText (/Батька/, msg => {
     var id = msg.from.id;
-    bot.sendMessage(msg.chat.id, 'Я обворовал магазин, что мне делать?')
+    bot.sendMessage(msg.chat.id, 'Я обворовал магазин, что мне делать?')})
 bot.onText (/Гейб/, msg => {
     var id = msg.from.id;
-    bot.sendMessage(msg.chat.id, 'CAADAgADIAAD4KV4BXGhwOaE5M7QAg')
+    bot.sendMessage(msg.chat.id, 'CAADAgADIAAD4KV4BXGhwOaE5M7QAg')})
 
 
 })
