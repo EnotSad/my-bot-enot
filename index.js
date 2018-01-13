ad91c659abc6d457269fd3069b78d188df0f1116
EnotSad <35348681+EnotSad@users.noreply.github.com>
2018-01-13 12:22:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -31,4 +31,25 @@ bot.onText (/Гейб/, msg => {
     bot.sendMessage(msg.chat.id, 'CAADAgADIAAD4KV4BXGhwOaE5M7QAg')})
 
 
+bot.onText(//напомни (.+) в (.+)/, function (msg, match) {
+    var userId = msg.from.id;
+    var text = match[1];
+    var time = match[2];
+
+    notes.push( { 'uid':userId, 'time':time, 'text':text } );
+
+    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
+});
+
+setInterval(function(){
+    for (var i = 0; i < notes.length; i++){
+        var curDate = new Date().getHours() + ':' + new Date().getMinutes();
+            if ( notes{{i}}['time'] == curDate ) {
+                bot.sendMessage(notes{{i}}['uid'], 'Напоминаю, что вы должны: '+ notes{{i}}['text'] + ' сейчас.');
+                notes.splice(i,1);
+            }
+        }
+},1000);
+
+
 
