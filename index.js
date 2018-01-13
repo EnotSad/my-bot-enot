1f97bd99a10890715943eba39d1f89901fae812d
EnotSad <35348681+EnotSad@users.noreply.github.com>
2018-01-13 11:34:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var bot = new TelegramBot (TOKEN, {polling: true});
 
 
 
-bot.onText (/"/start", "Привет", "Старт", "Эй"/, msg => {
+bot.onText (/"Привет", "Старт", "Эй"/, msg => {
     var id = msg.from.id;
     bot.sendMessage(msg.chat.id, 'Привет, как тебя зовут')
 bot.onText (/БГУИР/, msg => {
