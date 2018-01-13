142b83ef79c7c8291302b3232bf593932272dcb4
EnotSad <35348681+EnotSad@users.noreply.github.com>
2018-01-13 11:25:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ var TelegramBot = require('node-telegram-bot-api')
 
 var bot = new TelegramBot (TOKEN, {polling: true});
 
-var chel = bot.onText
 
 
-if chel in (Привет):
+bot.onText ('Привет', msg => {
     var id = msg.from.id;
     bot.sendMessage(msg.chat.id, 'Привет, как тебя зовут')
 })
